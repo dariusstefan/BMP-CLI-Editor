0ad290eaf283a0062d612d9a42572927625a62f3
dariusstefan <94108614+dariusstefan@users.noreply.github.com>
2022-10-19 18:37:50 +0300

    Forgot to run the Makefile.

--- test_script.py
+++ test_script.py
@@ -1,7 +1,7 @@
 import os
 
-os.system('make');
-os.system('mkdir -p output');
+os.system('make')
+os.system('mkdir -p output')
 
 for i in range(1, 5):
     cmd = 'mkdir -p output/Task{}'.format(i)
@@ -11,5 +11,5 @@ os.system('./bmp_editor < ./input/Task1/input0.txt')
 
 for i in range(2, 5):
     for j in range(0, 4):
-        cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(i,j);
+        cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(i, j)
         os.system(cmd)
