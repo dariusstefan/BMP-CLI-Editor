128bcfcf8d8e1f67a05dd308fa6529f55b29e555
dariusstefan <94108614+dariusstefan@users.noreply.github.com>
2022-10-23 17:12:22 +0300

    Added options to run only one task or to delete the output folder..

--- test_script.py
+++ test_script.py
@@ -1,15 +1,40 @@
 import os
 
-os.system('make')
-os.system('mkdir -p output')
+option = input("Choose between:\n\t1. all_tests\n\t2. single_test\n\t3. delete_output\n")
 
-for i in range(1, 5):
-    cmd = 'mkdir -p output/Task{}'.format(i)
+if option == 1:
+    os.system('make')
+    os.system('mkdir -p output')
+
+    for i in range(1, 6):
+        cmd = 'mkdir -p output/Task{}'.format(i)
+        os.system(cmd)
+
+    os.system('./bmp_editor < ./input/Task1/input0.txt')
+    print("\n")
+
+    for i in range(2, 6):
+        for j in range(0, 5):
+            cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(i, j)
+            os.system(cmd)
+
+elif option == 2:
+    task = input("Choose what test to do:\n")
+    os.system('make')
+    os.system('mkdir -p output')
+
+    cmd = 'mkdir -p output/Task{}'.format(task)
     os.system(cmd)
 
-os.system('./bmp_editor < ./input/Task1/input0.txt')
-
-for i in range(2, 5):
-    for j in range(0, 4):
-        cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(i, j)
+    if task == 1:
+        cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(task, 0)
         os.system(cmd)
+    else:
+        for j in range(0, 5):
+            cmd = './bmp_editor < ./input/Task{}/input{}.txt'.format(task, j)
+            os.system(cmd)
+
+else:
+    cmd = "rm -r ./output"
+    os.system(cmd)
+    print("All output deleted!")
